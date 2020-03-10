703301a178d51063e61bd57768184f2ec6e926ef
Cyril COLIN <colin.cyril@protonmail.com>
2020-03-10 22:18:35 +0100

    #22 improve search form

--- front/src/app/private/torrent-browser/torrent-browser.component.ts
+++ front/src/app/private/torrent-browser/torrent-browser.component.ts
@@ -8,6 +8,7 @@ import { TorrentDestination } from 'src/app/core/model/torrent-destination';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { tap } from 'rxjs/operators';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-torrent-browser',
@@ -20,38 +21,55 @@ export class TorrentBrowserComponent implements OnInit {
   torrentDestinations$: Observable<TorrentDestination[]>;
   form: FormGroup;
 
-  searchResult: BrowserTorrent[];
+  searchResult: BrowserTorrent[] = [];
   defaultDestination: TorrentDestination;
-
+  searching = false;
+  searchErrors: string[] = [];
   constructor(
     private proxyBrowserService: ProxyBrowserService,
     private proxyMonitoringService: ProxyMonitoringService,
     private mainToolbarService: MainToolbarService,
     private fb: FormBuilder,
     private router: Router,
+    private translate: TranslateService,
   ) { }
 
   ngOnInit(): void {
       this.mainToolbarService.setMainTitle('browser.mainTitle');
       this.torrentDestinations$ = this.proxyMonitoringService.getTorrentDestinations().pipe(
-        tap(res => this.defaultDestination = res.find(r => r.default))
+        tap(res => this.defaultDestination = res.find(r => r.default)),
+        tap(_ => {
+          this.form = this.fb.group({
+            search: this.fb.control(null, Validators.required),
+            destination: this.fb.control(this.defaultDestination, Validators.required),
+          });
+        })
       );
-
-      this.form = this.fb.group({
-        search: this.fb.control(null, Validators.required),
-        destination: this.fb.control(null, Validators.required),
-      });
   }
 
 
   search(): void {
-    const search = this.form.getRawValue();
-    this.proxyBrowserService.search(search).subscribe({
-      next: (res) => this.searchResult = res,
-      error: (err) => {
-        console.error(err);
-      }
-    });
+    this.searching = true;
+    this.searchErrors = [];
+    if (this.form.valid) {
+      const search = this.form.getRawValue();
+      this.proxyBrowserService.search(search).subscribe({
+        next: (res) => {
+          this.searching = false;
+          this.searchResult = res;
+        },
+        error: (err) => {
+          this.searching = false
+          this.searchErrors.push(this.translate.instant('browser.errors.search'));
+          console.error(err);
+        }
+      });
+    } else {
+      console.log(this.form);
+      this.searching = false;
+      this.searchErrors.push(this.translate.instant('browser.errors.searchNotValid'));
+    }
+
   }
 
   add(torrent: BrowserTorrent): void {
