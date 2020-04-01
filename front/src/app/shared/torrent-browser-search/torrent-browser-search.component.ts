1c0e6a50691de6cb90ed8c296727daef2d8bff38
Cyril COLIN <colin.cyril@protonmail.com>
2020-04-01 20:10:35 +0200

    #22 improve style of search component

--- front/src/app/shared/torrent-browser-search/torrent-browser-search.component.ts
+++ front/src/app/shared/torrent-browser-search/torrent-browser-search.component.ts
@@ -2,17 +2,34 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { TorrentDestination } from 'src/app/core/model/torrent-destination';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { SearchData } from 'src/app/core/services/proxy/proxy-browser/proxy-browser.service';
+import { state, style, transition, animate, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-torrent-browser-search',
   templateUrl: './torrent-browser-search.component.html',
-  styleUrls: ['./torrent-browser-search.component.scss']
+  styleUrls: ['./torrent-browser-search.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        overflow: 'hidden',
+        height: '*',
+      })),
+      state('out', style({
+        opacity: '0',
+        overflow: 'hidden',
+        height: '0px',
+      })),
+      transition('in => out', animate('400ms ease-in-out')),
+      transition('out => in', animate('400ms ease-in-out'))
+    ])
+  ]
 })
 export class TorrentBrowserSearchComponent implements OnInit {
   @Input() destinations: TorrentDestination[] = [];
   @Input() selected: TorrentDestination;
   @Output() searchEvent = new EventEmitter<SearchData>();
 
+  formAnimation: string;
   form: FormGroup;
 
   constructor(
@@ -20,6 +37,7 @@ export class TorrentBrowserSearchComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.formAnimation = 'in'
     this.form = this.fb.group({
       searchValue: this.fb.control(null),
       destination: this.fb.control(this.selected),
@@ -32,6 +50,11 @@ export class TorrentBrowserSearchComponent implements OnInit {
 
   search(): void {
     const searchData = this.form.getRawValue() as SearchData;
+    this.toggleForm();
     this.searchEvent.emit(searchData);
+  }
+
+  toggleForm(): void {
+    this.formAnimation = this.formAnimation === 'out' ? 'in' : 'out';
   }
 }
