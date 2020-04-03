bb874facb64ea581d9757a5771ee6c26cd719d1c
Cyril COLIN <colin.cyril@protonmail.com>
2020-04-03 21:09:15 +0200

    #22 show available space in top bar

--- front/src/app/shared/main-toolbar/main-toolbar.component.ts
+++ front/src/app/shared/main-toolbar/main-toolbar.component.ts
@@ -4,6 +4,9 @@ import { Title } from '@angular/platform-browser';
 import { MainToolbarService } from './main-toolbar.service';
 import { Observable } from 'rxjs';
 import { CurrentUserService } from 'src/app/core/services/current-user/current-user.service';
+import { DiskStatus } from 'src/app/core/model/disk-status.model';
+import { ProxyMonitoringService } from 'src/app/core/services/proxy/proxy-monitoring/proxy-monitoring.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-toolbar',
@@ -11,10 +14,13 @@ import { CurrentUserService } from 'src/app/core/services/current-user/current-u
   styleUrls: ['./main-toolbar.component.scss']
 })
 export class MainToolbarComponent implements OnInit {
+  diskStatus$: Observable<DiskStatus[]>;
   constructor(
     private sideMenuService: SideMenuService,
     private titleService: MainToolbarService,
     private currentUserService: CurrentUserService,
+    private proxyMonitoringService: ProxyMonitoringService,
+    private router: Router,
   ) { }
 
   get title(): Observable<string> {
@@ -25,6 +31,11 @@ export class MainToolbarComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.diskStatus$ = this.proxyMonitoringService.getDiskUsage();
+  }
+
+  goToMonitoring() {
+    this.router.navigate(['private', 'monitoring']);
   }
 
   isLogged() {
